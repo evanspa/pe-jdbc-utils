181cd1bd087776312566e9d5f56459aa4cb2c8e1
Paul Evans <evansp2@gmail.com>
2015-11-04 14:35:35 -0500

    Version 0.0.19-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-jdbc-utils "0.0.18"
+(defproject pe-jdbc-utils "0.0.19-SNAPSHOT"
   :description "A Clojure library of helper functions when working with JDBC."
   :url "https://github.com/evanspa/pe-jdbc-utils"
   :license {:name "MIT"
